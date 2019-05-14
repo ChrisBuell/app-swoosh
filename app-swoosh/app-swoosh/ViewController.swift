605c6534b153e8304df65f701aef5be401d31d3d
Christine Buell <cb@tinylzrd.com>
2019-05-14 17:41:52 -0500

    worked on background

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
+//        var w = UIScreen.main.bounds.width
+//        var h = UIScreen.main.bounds.height
+//        bgImg.frame = view.frame;
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+      
+        
+        
+       
+        
         // Do any additional setup after loading the view.
     }
 
